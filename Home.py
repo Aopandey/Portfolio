bfda217c19f6821e1f9d07137bc5ba653c100e1a
Avinash <avkr258@gmail.com>
2024-07-25 11:37:45 -0400

    Update in general

--- Home.py
+++ Home.py
@@ -40,7 +40,7 @@ with col2:
     zx.write("Email:", email)
     zx.write("#")
     zx.write("🌟Explore the website to learn more about the projects I've done to demonstrate my Skills. "
-             "Feel free to contact me!🌟")
+                 "Feel free to contact me!🌟")
 
 zx.write("#")
 zx.subheader("Professional Platforms")
@@ -53,44 +53,9 @@ zx.write("#")
 zx.subheader("Technical Skills")
 zx.write("---")
 zx.write("""
-- Languages: Python, C, C++, JavaScript, CSS, Java, R/R-Studio, SQL
-- Framework and Libraries: LangChain, Haystack, ZenML, Prefect, React, Redux, Express, Django, 
-    Flask, PySimpleGUI, Pandas, Paperetl, GROBID, PyTorch, TensorFlow, GitHub, AWS, Microsoft Azure, 
-    PyCharm, Jupyter
-- Database Management: MySQL, MongoDB, Microsoft SQL Server, Azure Data Studio
-""")
-
-# Concepts Learning
-zx.write("#")
-zx.subheader("Concepts Learning")
-zx.write("---")
-zx.write("""
-- Advanced Data Pipelines: Building scalable, DAG-based pipelines to manage and process large datasets.
-- Metadata Filtering Algorithms: Improving precision in summarized research outputs.
-- Automated Data Processing: Using tools like ZenML and Prefect for efficient data handling.
-- Streamlit Applications: Developing user-friendly web applications for data visualization and interaction.
-""")
-
-# Work Experience
-zx.write("#")
-zx.subheader("Work Experience")
-zx.write("---")
-zx.write("""
-**Machine Learning Engineering Intern**  
-Aider Ventures, Indianapolis, IN  
-*July 2024 – Present*
-
-- Developed automated pipelines using Haystack to fetch and summarize trending research papers.
-- Engineered scalable data pipelines leveraging ZenML and Prefect Python to analyze and classify research papers.
-- Implemented metadata filtering algorithms in LangChain, improving the precision of summarized research outputs.
-- Built scalable, DAG-based pipelines for large dataset processing.
-- Deployed a Streamlit-based application to showcase the developed application.
-
-**Business Analyst Intern – Office of Technology Services**  
-Legislative Services Agency, Indianapolis, IN  
-*December 2023 – March 2024*
-
-- Collaborated with software developers to support & troubleshoot critical software.
-- Executed advanced data verification and timeliness assurance processes for the Indiana General Assembly.
-- Facilitated strategic interactions and technical support with Indiana lawmakers and Legislative Services Agency staff.
-""")
+    - Languages: Python, C, C++, JavaScript, CSS, Java, R/R-Studio, SQL
+    - Framework and Libraries: LangChain, Haystack, ZenML, Prefect, React, Redux, Express, Django, 
+        Flask, PySimpleGUI, Pandas, Paperetl, GROBID, PyTorch, TensorFlow, GitHub, AWS, Microsoft Azure, 
+        PyCharm, Jupyter
+    - Database Management: MySQL, MongoDB, Microsoft SQL Server, Azure Data Studio
+    """)
