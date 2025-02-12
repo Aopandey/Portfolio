0ec6e68227b881c1cf32c6b83d305fead0500006
Aopandey <avkr258@gmail.com>
2025-02-12 00:14:18 -0500

    revamp v1

--- Home.py
+++ Home.py
@@ -118,11 +118,13 @@ st.markdown("""
 # --- PERSONAL INFORMATION (Header Section) ---
 name = "Avinash Pandey"
 description1 = ("""\
+<span style="font-size:1.2em;">
 Hi there! I am a <span style="color:#1E90FF; font-weight:bold;">Data & Machine Learning Engineer</span> with a <span style="color:#1E90FF; font-weight:bold;">B.S. in Computer Science</span> and a minor in Mathematics from <span style="color:#1E90FF; font-weight:bold;">Purdue University (Dec 2024)</span>, specializing in <span style="color:#FF6347; font-weight:bold;">AI-driven data pipelines, ETL automation, and scalable ML solutions</span>.
 
 At <span style="color:#1E90FF; font-weight:bold;">Aider Ventures</span>, I developed an automated ETL pipeline processing <span style="color:#FF6347; font-weight:bold;">10,000+ research papers</span>, integrating <span style="color:#FF6347; font-weight:bold;">FAISS embeddings, ChromaDB, and LangChain</span> to enhance retrieval accuracy and metadata extraction. Previously, at <span style="color:#1E90FF; font-weight:bold;">Legislative Services Agency</span>, I designed SQL-based ETL workflows and Power BI dashboards, achieving <span style="color:#FF6347; font-weight:bold;">98% data accuracy</span> in legislative data processing.
 
 My expertise includes <span style="color:#FF6347; font-weight:bold;">Python, SQL, Cloud Computing (AWS, Docker, Kubernetes)</span> and <span style="color:#FF6347; font-weight:bold;">Machine Learning (Scikit-learn, PyTorch, TensorFlow)</span>. I thrive at bridging AI, data engineering, and business intelligence to drive actionable insights and scalable AI solutions.
+</span>
 """)
 social_media = {
     "Email": {
