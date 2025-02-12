a08b1b025a62157d6593af826844a7a11c62570d
Aopandey <avkr258@gmail.com>
2025-02-12 13:22:53 -0500

    revamp v2

--- pages/about.py
+++ pages/about.py
@@ -64,7 +64,7 @@ def show_about():
 
     # List of image file paths (ensure these files exist in the specified location)
     image_paths = [
-        "hobby1.jpg",
+        "images/hobby1.jpg",
         "images/hobby2.jpg",
         "images/hobby3.jpg",
         "images/hobby4.jpg",
