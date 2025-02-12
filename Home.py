f3d8d5059df7072769790e6c1ca1612b8af849d0
Aopandey <avkr258@gmail.com>
2025-02-12 11:37:43 -0500

    revamp v1

--- Home.py
+++ Home.py
@@ -69,7 +69,7 @@ profile_pic = Image.open("images/photo.jpg")
 # Right column: Description (aligned at the top)
 col_left, col_right = st.columns([1, 2])
 with col_left:
-    st.markdown("<div style='margin-left: 400px;'><h1 style='text-align: center;'>{}</h1></div>".format(name),
+    st.markdown("<div style='margin-left: 350px;'><h1 style='text-align: center;'>{}</h1></div>".format(name),
                 unsafe_allow_html=True)
     spacer, image_col = st.columns([1, 1])
     with image_col:
