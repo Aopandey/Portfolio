367c262e527ff96322318bbcc6e284799d07bc52
Aopandey <avkr258@gmail.com>
2025-02-12 11:58:05 -0500

    revamp v2

--- Home.py
+++ Home.py
@@ -8,7 +8,7 @@ import streamlit as st
 from PIL import Image
 import importlib.util
 
-spec = importlib.util.spec_from_file_location("projects", "pages")
+spec = importlib.util.spec_from_file_location("projects", "01_Python Projects.py")
 projects = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(projects)
 
