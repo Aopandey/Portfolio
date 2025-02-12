1161e6c27609f808da2de13c73a0f1916df44836
Aopandey <avkr258@gmail.com>
2025-02-12 00:28:16 -0500

    revamp v1

--- Home.py
+++ Home.py
@@ -162,7 +162,7 @@ with col_left:
     st.markdown("<h1 style='text-align: center;'>{}</h1>".format(name), unsafe_allow_html=True)
     spacer, image_col = st.columns([1, 4.5])
     with image_col:
-        st.image(profile_pic, width=300)
+        st.image(profile_pic, width=500)
 with col_right:
     st.markdown("<div style='margin-left: 100px; margin-right: 400px; margin-top: 80px;'>{}</div>".format(description1), unsafe_allow_html=True)
 
